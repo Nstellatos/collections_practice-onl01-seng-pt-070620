0af3941142b5783a55be101d68188bfd0430a963
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-16 06:06:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
-
+def sort_array_asc(num)
+  num.sort
+end
+def sort_array_desc(array)
+  array.sort {|a,b| -(a <=>b)}
+end
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+def swap_elements(array)
+  array[1], array[2] = array [2], array[1]
+  array
+end
+def reverse_array(array)
+  array.reverse
+end
